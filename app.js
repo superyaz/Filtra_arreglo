2d501dc624f6804c5ce15d545a74f99ce4cb2392
superyaz <yacamocursos@hotmail.com>
2019-05-20 23:03:15 -0500

    Función para retornar arreglo con valores mayores a 10

---
+++ app.js
@@ -0,0 +1,14 @@
+function filter (array){
+   let cont = [];
+    for (let i=0; i< array.length; i++){
+        if(array[i] > 10 ){
+           cont.push(array[i]);
+           
+        }
+    }
+    return [cont];
+}
+
+
+let resultado = filter([3, 12, 45, 7]);
+console.log(resultado);
